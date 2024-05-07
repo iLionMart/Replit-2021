8969472bedb673b4fc051937bb8d81c3d04ea431
Lion Mart <45979134+iLionMart@users.noreply.github.com>
2024-05-07 17:21:46 -0600

    Create 09-MetodoConstructor.java

---
+++ 09-MetodoConstructor.java
@@ -0,0 +1,11 @@
+public class main{
+  public static void main(String [] args){
+    Persona per1 = new Persona ("Lucas", 24);
+    Persona per2 = new Persona ("Guillermo", 63);
+    Persona per3 = new Persona ("Miguel", 8);
+
+    per1.mostrarDatos();
+    per2.mostrarDatos();
+    per3.mostrarDatos();
+  }
+}
